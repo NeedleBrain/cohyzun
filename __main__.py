3b3315ecc3b678839b413c949551bc417bfe313e
shubham <shubhamchauhan1004@gmail.com>
2017-10-24 12:08:17 +0530

    edited

--- __main__.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-if __name__ == '__main__':
-	import importlib
-
-    if '--version' in sys.argv:
-	    chatterbot = importlib.import_module('scatter')
-	    print(chatterbot.__version__)
